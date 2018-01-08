e8ff8cc4a7e3590a93c5f053929878b120a18e89
vrigos90 <vrigos90@gmail.com>
2018-01-08 18:35:34 +0100

    Merge branch 'master' of https://github.com/vrigos90/Stastki_v_006
    # Conflicts:
    #	src/Statki/Main.java

--- src/Statki/Main.java
+++ src/Statki/Main.java
@@ -22,6 +22,7 @@ import javafx.scene.image.*;
 
 import Statki.Plansza.Pole;
 import Statki.Plansza.Pole.*;
+import java.util.Random;
 
 import static javafx.scene.input.MouseEvent.*;
 
@@ -38,34 +39,102 @@ public class Main extends Application {
     VBox planszaK;
     int typ = 4;
     int ilosc = 1;
+  
+    int[][] Komputer = new int[12][12];
+    
 
     public boolean rozpoczecie = false;
 
-    
-    boolean sprawdz(int x,int y, int dl, boolean poziomo){
+    void losuj_statek(int dl){
+        Random r = new Random();
+
+        int K_los = r.nextInt(2);
+
+        // Statek poziomo
+        if(K_los == 0)
+        {
+            int X_los = r.nextInt(11-dl)+1;
+            int Y_los = r.nextInt(10)+1;
+
+            while(sprawdz(X_los,Y_los,dl,true,planszaK) == false)
+            {
+                X_los = r.nextInt(11-dl)+1;
+                Y_los = r.nextInt(10)+1;
+            }
+
+            // Deklarowanie statku (4-pola)
+            for(int i=0;i<dl;i++) {
+                Komputer[X_los+i][Y_los] = 1;
+                getPole(X_los+i,Y_los,planszaK).setFill(Color.BROWN);
+            }
+            //new Statek(X_los,Y_los,dl,true,this,planszaK);
+
+
+            // Statek pionowo
+        }else if(K_los == 1){
+            int X_los = r.nextInt(10)+1;
+            int Y_los = r.nextInt(11-dl)+1;
+
+            while(sprawdz(X_los,Y_los,dl,false,planszaK) == false)
+            {
+                X_los = r.nextInt(10)+1;
+                Y_los = r.nextInt(11-dl)+1;
+            }
+
+            //Deklarowanie statku
+
+            for(int i=0;i<dl;i++)
+            {
+                Komputer[X_los][Y_los+i] = 1;
+                getPole(X_los,Y_los+i,planszaK).setFill(Color.BROWN);
+            }
+            //new Statek(X_los,Y_los,dl,false,this,planszaK);
+
+
+
+        }
+
+
+    }
+
+    public void ustawStatkiK(){
+
+        losuj_statek(4);
+
+        for(int i =0 ;i<2;i++)
+        losuj_statek(3);
+
+        for(int i =0 ;i<3;i++)
+        losuj_statek(2);
+
+        for(int i =0 ;i<4;i++)
+        losuj_statek(1);
+    }
+   
+    boolean sprawdz(int x,int y, int dl, boolean poziomo,VBox plansza){
         
         if(poziomo){
             for(int i=0;i<dl;i++){
-                if(this.getPole(x+i, y).getFill()==Color.BROWN || this.getPole(x+i, y) == null)
+                if(this.getPole(x+i, y,plansza).getFill()==Color.BROWN || this.getPole(x+i, y,plansza) == null)
                     return false;
                 else{
-                    if(this.getPole(x+i,y-1).getFill()==Color.BROWN||this.getPole(x+i,y+1).getFill()==Color.BROWN)
+                    if(this.getPole(x+i,y-1,plansza).getFill()==Color.BROWN||this.getPole(x+i,y+1,plansza).getFill()==Color.BROWN)
                         return false;
                     else{
-                        if(this.getPole(x-1,y).getFill()==Color.BROWN||this.getPole(x-1,y-1).getFill()==Color.BROWN||this.getPole(x-1,y+1).getFill()==Color.BROWN||this.getPole(x+dl,y).getFill()==Color.BROWN||this.getPole(x+dl,y-1).getFill()==Color.BROWN||this.getPole(x+dl,y+1).getFill()==Color.BROWN)
+                        if(this.getPole(x-1,y,plansza).getFill()==Color.BROWN||this.getPole(x-1,y-1,plansza).getFill()==Color.BROWN||this.getPole(x-1,y+1,plansza).getFill()==Color.BROWN||this.getPole(x+dl,y,plansza).getFill()==Color.BROWN||this.getPole(x+dl,y-1,plansza).getFill()==Color.BROWN||this.getPole(x+dl,y+1,plansza).getFill()==Color.BROWN)
                             return false;
                     }
                 }
             }
         }else{
             for(int i=0;i<dl;i++){
-                if(this.getPole(x,y+i).getFill()==Color.BROWN)
+                if(this.getPole(x,y+i,plansza).getFill()==Color.BROWN)
                     return false;
                 else{
-                    if(this.getPole(x-1,y+i).getFill()==Color.BROWN||this.getPole(x+1,y+i).getFill()==Color.BROWN)
+                    if(this.getPole(x-1,y+i,plansza).getFill()==Color.BROWN||this.getPole(x+1,y+i,plansza).getFill()==Color.BROWN)
                         return false;
                     else{
-                        if(this.getPole(x,y-1).getFill()==Color.BROWN||this.getPole(x-1,y-1).getFill()==Color.BROWN||this.getPole(x+1,y-1).getFill()==Color.BROWN||this.getPole(x,y+dl).getFill()==Color.BROWN||this.getPole(x-1,y+dl).getFill()==Color.BROWN||this.getPole(x+1,y+dl).getFill()==Color.BROWN)
+                        if(this.getPole(x,y-1,plansza).getFill()==Color.BROWN||this.getPole(x-1,y-1,plansza).getFill()==Color.BROWN||this.getPole(x+1,y-1,plansza).getFill()==Color.BROWN||this.getPole(x,y+dl,plansza).getFill()==Color.BROWN||this.getPole(x-1,y+dl,plansza).getFill()==Color.BROWN||this.getPole(x+1,y+dl,plansza).getFill()==Color.BROWN)
                             return false;
                     }
                 }
@@ -79,7 +148,7 @@ public class Main extends Application {
         if(poz)
         {
             for (int i = 0; i < typ; i++) {
-                Pole p4 = getPole(p3.x, p3.y);
+                Pole p4 = getPole(p3.x, p3.y,planszaG);
                 p3.x++;
                 p4.setFill(Color.BROWN);
                 p4.setStroke(Color.BLACK);
@@ -87,7 +156,7 @@ public class Main extends Application {
         }else
         {
             for (int i = 0; i < typ; i++) {
-                Pole p4 = getPole(p3.x, p3.y);
+                Pole p4 = getPole(p3.x, p3.y,planszaG);
                 p3.y++;
                 p4.setFill(Color.BROWN);
                 p4.setStroke(Color.BLACK);
@@ -112,16 +181,22 @@ public class Main extends Application {
 
                 c.addEventFilter(MouseEvent.MOUSE_PRESSED, event ->
                 {
-                   Plansza.Pole p = (Plansza.Pole)event.getSource();
-                   if(sprawdz(p.x,p.y,typ,event.getButton() == MouseButton.PRIMARY))
-                    new Statek(typ,event.getButton() == MouseButton.PRIMARY,event,this);
-                   else
-                       ilosc--;
-                    if((typ==4&&ilosc==1)||(typ==3&&ilosc==2)||(typ==2&&ilosc==3)||(typ==1&&ilosc==4)){
-                        typ--;
-                        ilosc=0;
-                    }
-                        ilosc++;
+                    try{
+                                Plansza.Pole p = (Plansza.Pole)event.getSource();
+                           if(sprawdz(p.x,p.y,typ,event.getButton() == MouseButton.PRIMARY,planszaG))
+                            new Statek(typ,event.getButton() == MouseButton.PRIMARY,event,this);
+                           else
+                               ilosc--;
+                            if((typ==4&&ilosc==1)||(typ==3&&ilosc==2)||(typ==2&&ilosc==3)||(typ==1&&ilosc==4)){
+                                typ--;
+                                ilosc=0;
+                            }
+                                ilosc++;
+                                
+                               
+                                
+                    }catch(Exception e){}
+                   
                 });
                 }
             
@@ -140,8 +215,6 @@ public class Main extends Application {
 
         planszaK = new VBox();
 
-        layoutX = 0.5;
-        layoutY = 0.5;
 
         for (int y = 0; y < 12; y++) {
             HBox row = new HBox();
@@ -151,10 +224,22 @@ public class Main extends Application {
                 Pole c = new Pole(x, y);
                 if(x==0||x==11)
                     c.setVisible(false);
-                row.getChildren().add(c);
+                
+               c.addEventFilter(MouseEvent.MOUSE_PRESSED, event ->{
+
+                        Pole p = (Pole)event.getSource();
+
+                        if(Komputer[p.x][p.y] == 1)
+                            getPole(p.x,p.y,planszaK).setFill(Color.RED);
+                        else
+                            getPole(p.x,p.y,planszaK).setFill(Color.BLACK);
+                    });
+                
+                row.getChildren().add(c);              
             }
 
             planszaK.getChildren().add(row);
+        
         }
 
         planszaK.setLayoutX(450.0);
@@ -189,8 +274,8 @@ public class Main extends Application {
         planszaII.setSmooth(false);
     }
 
-    public Pole getPole(int x, int y) {
-        return (Pole)((HBox)planszaG.getChildren().get(y)).getChildren().get(x);
+    public Pole getPole(int x, int y,VBox plansza) {
+        return (Pole)((HBox)plansza.getChildren().get(y)).getChildren().get(x);
     }
 
     private Parent gvk(){
@@ -202,13 +287,13 @@ public class Main extends Application {
         dodajPlanszeGracz();
         dodajPlanszeKomputer();
 
-        System.out.println(getPole(2,3));
 
         root.getChildren().add(tlo);
         root.getChildren().add(planszaI);
         root.getChildren().add(planszaII);
         root.getChildren().add(planszaG);
         root.getChildren().add(planszaK);
+        ustawStatkiK();
 
         return root;
     }
@@ -222,7 +307,6 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
--- src/Statki/Statek.java
+++ src/Statki/Statek.java
@@ -24,26 +24,26 @@ public class Statek extends Parent{
         
         if(poziomo){
             for(int i=0;i<dl;i++){
-                if(plansza.getPole(x+i, y).getFill()==Color.BROWN || plansza.getPole(x, y) == null)
+                if(plansza.getPole(x+i, y,plansza.planszaG).getFill()==Color.BROWN || plansza.getPole(x, y,plansza.planszaG) == null)
                     return false;
                 else{
-                    if(plansza.getPole(x+i,y-1).getFill()==Color.BROWN||plansza.getPole(x+i,y+1).getFill()==Color.BROWN)
+                    if(plansza.getPole(x+i,y-1,plansza.planszaG).getFill()==Color.BROWN||plansza.getPole(x+i,y+1,plansza.planszaG).getFill()==Color.BROWN)
                         return false;
                     else{
-                        if(plansza.getPole(x-1,y).getFill()==Color.BROWN||plansza.getPole(x-1,y-1).getFill()==Color.BROWN||plansza.getPole(x-1,y+1).getFill()==Color.BROWN||plansza.getPole(x+dl,y).getFill()==Color.BROWN||plansza.getPole(x+dl,y-1).getFill()==Color.BROWN||plansza.getPole(x+dl,y+1).getFill()==Color.BROWN)
+                        if(plansza.getPole(x-1,y,plansza.planszaG).getFill()==Color.BROWN||plansza.getPole(x-1,y-1,plansza.planszaG).getFill()==Color.BROWN||plansza.getPole(x-1,y+1,plansza.planszaG).getFill()==Color.BROWN||plansza.getPole(x+dl,y,plansza.planszaG).getFill()==Color.BROWN||plansza.getPole(x+dl,y-1,plansza.planszaG).getFill()==Color.BROWN||plansza.getPole(x+dl,y+1,plansza.planszaG).getFill()==Color.BROWN)
                             return false;
                     }
                 }
             }
         }else{
             for(int i=0;i<dl;i++){
-                if(plansza.getPole(x,y+i).getFill()==Color.BROWN)
+                if(plansza.getPole(x,y+i,plansza.planszaG).getFill()==Color.BROWN)
                     return false;
                 else{
-                    if(plansza.getPole(x-1,y+i).getFill()==Color.BROWN||plansza.getPole(x+1,y+i).getFill()==Color.BROWN)
+                    if(plansza.getPole(x-1,y+i,plansza.planszaG).getFill()==Color.BROWN||plansza.getPole(x+1,y+i,plansza.planszaG).getFill()==Color.BROWN)
                         return false;
                     else{
-                        if(plansza.getPole(x,y-1).getFill()==Color.BROWN||plansza.getPole(x-1,y-1).getFill()==Color.BROWN||plansza.getPole(x+1,y-1).getFill()==Color.BROWN||plansza.getPole(x,y+dl).getFill()==Color.BROWN||plansza.getPole(x-1,y+dl).getFill()==Color.BROWN||plansza.getPole(x+1,y+dl).getFill()==Color.BROWN)
+                        if(plansza.getPole(x,y-1,plansza.planszaG).getFill()==Color.BROWN||plansza.getPole(x-1,y-1,plansza.planszaG).getFill()==Color.BROWN||plansza.getPole(x+1,y-1,plansza.planszaG).getFill()==Color.BROWN||plansza.getPole(x,y+dl,plansza.planszaG).getFill()==Color.BROWN||plansza.getPole(x-1,y+dl,plansza.planszaG).getFill()==Color.BROWN||plansza.getPole(x+1,y+dl,plansza.planszaG).getFill()==Color.BROWN)
                             return false;
                     }
                 }
@@ -52,7 +52,7 @@ public class Statek extends Parent{
         return true;
     }
 
-    public Statek(Integer typ, boolean poz,  MouseEvent event, Main planszaG){
+    public Statek(Integer typ, boolean poz,  MouseEvent event, Main plansza){
         this.typStatku = typ;
         this.poziomo = poz;
         
@@ -61,24 +61,23 @@ public class Statek extends Parent{
         
         if(poz)
         {
-            if(sprawdz(p3.x,p3.y,typ,true,planszaG))
+            if(sprawdz(p3.x,p3.y,typ,true,plansza))
             for (int i = 0; i < typ; i++) {
-                Plansza.Pole p4 = planszaG.getPole(p3.x,p3.y);
+                Plansza.Pole p4 = plansza.getPole(p3.x,p3.y,plansza.planszaG);
                 p3.x++;
                 p4.setFill(Color.BROWN);
                 p4.setStroke(Color.BLACK);
             }
         }else
         {
-           if(sprawdz(p3.x,p3.y,typ,false,planszaG))
+           if(sprawdz(p3.x,p3.y,typ,false,plansza))
             for (int i = 0; i < typ; i++) {
-                Plansza.Pole p4 = planszaG.getPole(p3.x, p3.y);
+                Plansza.Pole p4 = plansza.getPole(p3.x, p3.y,plansza.planszaG);
                 p3.y++;
                 p4.setFill(Color.BROWN);
                 p4.setStroke(Color.BLACK);
             }
         }
     }
-
 
 }
