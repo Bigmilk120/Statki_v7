8cde61cdd29f47452863ccee2f17eeb1b5635af8
vrigos90 <vrigos90@gmail.com>
2018-01-10 21:41:13 +0100

    Merge branch 'master' of https://github.com/Bigmilk120/Statki_v7
    # Conflicts:
    #	src/Statki/Main.java

--- src/MenuGlowne/Main.java
+++ src/MenuGlowne/Main.java
@@ -11,14 +11,12 @@ public class Main extends Application  {
     @Override
     public void start(Stage primaryStage) throws Exception{
         
-        try{
+
         Parent root = FXMLLoader.load(getClass().getResource("main_menu.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
-        }finally{
-            primaryStage.close();
-        }
+        
     }
 
 
